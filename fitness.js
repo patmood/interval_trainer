29f9137a4cf3b45f2604855e572b3eb65266b42c
patmood <patrick.n.moody@gmail.com>
2013-10-01 15:46:01 -0700

    reset button

--- fitness.js
+++ fitness.js
@@ -1,27 +1,29 @@
 var welcome = document.getElementById('welcome')
 var clock = document.getElementById('counter')
 var body = document.getElementsByTagName('body')[0]
-var skipButton = document.getElementById('skip')
 var description = document.getElementById('description')
+var controls = document.getElementById('controls')
 var workInterval = 5
 var restInterval = 3
-var exercises = ['Jumping Jacks', 'Wall Sit', 'Push-ups', 'Sit-ups','Step-ups', 'Squats', 'Tricep Dips', 'Plank', 'High Knees', 'Lunges', 'Push-ups and Rotation', 'Side Plank']
-var skip = false
+// var exercises = ['Jumping Jacks', 'Wall Sit', 'Push-ups', 'Sit-ups','Step-ups', 'Squats', 'Tricep Dips', 'Plank', 'High Knees', 'Lunges', 'Push-ups and Rotation', 'Side Plank']
+var exercises = ['Jumping Jacks', 'Wall Sit']
+var skip
 var current
+var timeout
 var beep = new Audio("beep.wav")
 
 
 var counter = document.getElementById('counter')
 
 function countDown(i, nextAction){
-  if (i < 0 || skip) {
+  if (i <= 0 || skip) {
     skip = false
     beep.play()
     nextAction()
     return
   }
   counter.innerHTML = i
-  setTimeout(function(){
+  timeout = setTimeout(function(){
     countDown(i-1,nextAction)
   },1000)
 }
@@ -30,34 +32,39 @@ function reset(){
   welcome.style.display = 'block'
   counter.style.display = 'none'
   description.style.display = 'none'
-  skipButton.style.display = 'none'
+  controls.style.display = 'none'
+  body.style.background = "#3498db"
+  clearTimeout(timeout)
+  current = 0
+  skip = true
 }
 
 
 function startWorkout(){
+  description.style.display = 'block'
   welcome.style.display = 'none'
   counter.style.display = 'block'
-  skipButton.style.display = 'block'
-  current = -1
+  controls.style.display = 'block'
+  current = 0
+  skip = false
   rest()
 }
 
 function nextWorkout(){
-  if (exercises[current]) {
-    body.style.background = "#e74c3c"
-    description.innerHTML = exercises[current]
-    countDown(workInterval, rest)
-  } else {
-    reset()
-  }
+  if (!exercises[current]) reset()
+  body.style.background = "#e74c3c"
+  description.innerHTML = exercises[current]
+  countDown(workInterval, rest)
+  current++
 }
 
 function rest(){
-  current++
+  if (!exercises[current]) reset()
   body.style.background = "#3498db"
   description.innerHTML = "Up Next:<br>"+exercises[current]
   countDown(restInterval, nextWorkout)
 }
 
 start.addEventListener('click',startWorkout)
-skipButton.addEventListener('click',function(){ skip = true })
+document.getElementById('skip').addEventListener('click', function(){ skip = true })
+document.getElementById('reset').addEventListener('click', reset)
