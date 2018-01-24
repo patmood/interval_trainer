bb220b1cc7b51d8de2954ba769072e8f09c3f577
patmood <patrick.n.moody@gmail.com>
2018-01-23 18:23:32 -0800

    formatting + made audio path relative

--- fitness.js
+++ fitness.js
@@ -8,17 +8,29 @@ var progressPoints = document.getElementById('progress').children
 var workInterval = 30
 var restInterval = 10
 // var exercises = ['Jumping Jacks', 'Wall Sit', 'Push-ups', 'Sit-ups','Step-ups', 'Squats', 'Tricep Dips', 'Plank', 'High Knees', 'Lunges', 'Push-ups and Rotation', 'Side Plank']
-var exercises = ['Burpees', 'Wall Sit', 'Push-ups', 'Sit-ups','Box Jumps', 'Squats', 'Tricep Dips', 'Plank', 'High Knees', 'Lunges', 'Close Grip Pushups', 'Stomach Hollow']
+var exercises = [
+  'Burpees',
+  'Wall Sit',
+  'Push-ups',
+  'Sit-ups',
+  'Box Jumps',
+  'Squats',
+  'Tricep Dips',
+  'Plank',
+  'High Knees',
+  'Lunges',
+  'Close Grip Pushups',
+  'Stomach Hollow',
+]
 
 var skip
 var current
 var timeout
-var beep = new Audio("beep.wav")
-
+var beep = new Audio('./beep.wav')
 
 var counter = document.getElementById('counter')
 
-function countDown(i, nextAction){
+function countDown(i, nextAction) {
   if (i <= 0 || skip) {
     skip = false
     beep.currentTime = 0
@@ -27,26 +39,25 @@ function countDown(i, nextAction){
     return
   }
   counter.innerHTML = i
-  timeout = window.setTimeout(function(){
-    countDown(i-1,nextAction)
-  },1000)
+  timeout = window.setTimeout(function() {
+    countDown(i - 1, nextAction)
+  }, 1000)
 }
 
-function reset(){
+function reset() {
   welcome.style.display = 'block'
   counter.style.display = 'none'
   description.style.display = 'none'
   controls.style.display = 'none'
   body.style.background = '#3498db'
-  for (var i = 0; i < progressPoints.length; i++){
+  for (var i = 0; i < progressPoints.length; i++) {
     progressPoints[i].className = ' '
   }
   skip = true
   current = 0
 }
 
-
-function startWorkout(){
+function startWorkout() {
   description.style.display = 'block'
   welcome.style.display = 'none'
   counter.style.display = 'block'
@@ -56,22 +67,33 @@ function startWorkout(){
   rest()
 }
 
-function nextWorkout(){
-  if (!exercises[current]) { reset(); return }
-  body.style.background = "#e74c3c"
+function nextWorkout() {
+  if (!exercises[current]) {
+    reset()
+    return
+  }
+  body.style.background = '#e74c3c'
   description.innerHTML = exercises[current]
   countDown(workInterval, rest)
   progressPoints[current].className = 'done'
   current++
 }
 
-function rest(){
-  if (!exercises[current]) { reset(); return }
-  body.style.background = "#3498db"
-  description.innerHTML = "Up Next:<br>"+exercises[current]
+function rest() {
+  if (!exercises[current]) {
+    reset()
+    return
+  }
+  body.style.background = '#3498db'
+  description.innerHTML = 'Up Next:<br>' + exercises[current]
   countDown(restInterval, nextWorkout)
 }
 
-start.addEventListener('click',startWorkout)
-document.getElementById('skip').addEventListener('click', function(){ skip = true })
-document.getElementById('reset').addEventListener('click', function(){ current = null; skip = true })
+start.addEventListener('click', startWorkout)
+document.getElementById('skip').addEventListener('click', function() {
+  skip = true
+})
+document.getElementById('reset').addEventListener('click', function() {
+  current = null
+  skip = true
+})
